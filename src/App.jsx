587fb8f611a4869492b5999ca91759c08a4f6e78
Lestarplay <162745949+Lestarplay@users.noreply.github.com>
2024-12-10 19:25:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Cronometro from "./components/Crônometro"
+
+function App() {
+  return(
+    <div className="App">
+      <Cronometro/>
+
+    </div>
+  )
+}
+
+export default App
